b382f9fecdabe369749928322c7c1f9b1d736ac0
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-09-19 14:32:24 +0800

    chore: first commit

---
+++ src/main/java/com/project/shoppingwise/ShoppingWiseApplication.java
@@ -0,0 +1,13 @@
+package com.project.shoppingwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWiseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShoppingWiseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/project/shoppingwise/ShoppingWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shoppingwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWiseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
